86c554a757dff752f9078b7bf7ebb1625f964ec3
Helen Chen <hechen@hmc.edu>
2024-02-22 15:56:45 -0800

    colorbar location, not position

--- hytraj/hyplot.py
+++ hytraj/hyplot.py
@@ -70,5 +70,5 @@ class ClusterPlot:
             xx, yy = m(lon1[tr].values, lat1[tr].values)
             m.plot(xx, yy, color=colors[kcount[count]], lw=lw, label = f'{prop*100:.2f}%')
         plt.legend(bbox_to_anchor=(1.01,1))
-        plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), label="# of back trajectories", position = 'bottom')
+        plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), label="# of back trajectories", location = 'bottom')
         return ax
