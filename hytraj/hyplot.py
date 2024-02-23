77ced7e4115f02d4712813180ce0c62508999f2d
Helen Chen <hechen@hmc.edu>
2024-02-22 16:36:12 -0800

    no legend?

--- hytraj/hyplot.py
+++ hytraj/hyplot.py
@@ -70,6 +70,6 @@ class ClusterPlot:
             lwd = lw #*(prop)
             xx, yy = m(lon1[tr].values, lat1[tr].values)
             m.plot(xx, yy, color=cmap(kcount[count]/np.sum(kcount)), lw=lw, label = f'{prop*100:.2f}%')
-        plt.legend(bbox_to_anchor=(1.01,1))
+        # plt.legend(bbox_to_anchor=(1.01,1))
         plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), label="# of back trajectories", location = 'bottom')
         return ax
