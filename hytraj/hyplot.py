df72288dec5a0cfa71b2884915d197b5ca331c8c
Helen Chen <hechen@hmc.edu>
2024-02-22 17:06:56 -0800

    update legend position

--- hytraj/hyplot.py
+++ hytraj/hyplot.py
@@ -70,6 +70,6 @@ class ClusterPlot:
             lwd = lw #*(prop)
             xx, yy = m(lon1[tr].values, lat1[tr].values)
             m.plot(xx, yy, color=colors[i], lw=lw, label = f'{prop*100:.2f}%')
-        plt.legend(bbox_to_anchor=(0.9,1.1), ncols = 3)
+        plt.legend(bbox_to_anchor=(0.9,0), ncols = 3)
         # plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), ax=plt.gca(), label="# of back trajectories", location = 'bottom')
         return ax
